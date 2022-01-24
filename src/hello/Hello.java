da0cec77c2d1991fb342e16e906e152452b8fdba
Tatiana Vasylyniuk <tatianavasylyniuk@gmail.com>
2022-01-24 10:27:40 -0500

    First statements in Hello class

--- src/hello/Hello.java
+++ src/hello/Hello.java
@@ -1,5 +1,9 @@
 package hello;
 
 public class Hello {
+    public static void main(String[] args){
+        System.out.println("Hello world");
+        System.out.println("We are learning git today");
+    }
 
 }
